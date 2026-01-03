b0aab52e00fb9a7aaae79d6ac45922ae8a10cabb
rohitsinghabad <rohitsinghrajputabad@gmail.com>
2026-01-03 22:55:27 +0530

    learned loop today

---
+++ app.js
@@ -0,0 +1,118 @@
+let prompt = require('prompt-sync') ();
+// let n = Number(prompt("Enter a Number "));
+
+// switch(day ) {
+//     case 1: {
+//         console.log("Monday");
+//         break
+//     }
+//     case 1: {
+//         console.log("Tuesday");
+//         break
+//     }
+//     case 1: {
+//         console.log("Wednesday");
+//         break
+//     }
+//     case 1: {
+//         console.log("Tursday");
+//         break
+//     }
+// default : console.log("Kuch bhi nhi")
+// }
+
+// Q. Check consonant and vowel using switch
+
+// let s = Number(prompt("Enter a string"))
+
+// let consonant = 0, vowel = 0;
+
+// for (let i= 0; i<s.length; i++) {
+//     let ch = s.charAt(i);
+//     switch(ch){
+        
+//     }
+// }
+
+// Q. WAP to calculate the area of circle, rectangle, triangle using switch
+
+
+// Q. Print hello world "n" times
+
+// let n = Number(prompt('Enter a number  '))
+
+// for (let i = 1; i<=n; i++) {
+//     console.log("hello world")
+// }
+
+// Q. print reverse no. from n to 1
+
+// let n = Number(prompt("Enter a number "))
+
+// for (i= n; i>=1; i--){
+//     console.log(i);
+    
+// }
+
+// Q. Take a no. input and print its table
+
+// let n = Number(prompt("Enter a number "))
+
+// for(let i =1; i<=10; i++) {
+//     console.log(n + " * " + i + " = " + (n*i));
+    
+// }
+
+// Q. sum up to n terms
+
+// let n = Number(prompt("ENTER A NO. "));
+// let sum = 0;
+// for(i= 1; i<=n; i++) {
+//     sum = sum + i;
+// }
+// console.log(sum)
+
+// Q. Factorial of given no.
+
+// let n = Number(prompt("Enter a number "))
+// let factorial = 1;
+
+// for(i = 1; i <=n; i++) {
+//     factorial= factorial* i;
+// }
+// console.log(factorial)
+
+// Q. Print the sum of all even & odd no. in a range separately
+
+// let a = Number(prompt("Enter a first Number "));
+// let b = Number(prompt("Enter a second Number "));
+// let evenSum = 0, oddSum= 0;
+
+// if (a>b) {
+//     let temp = a;
+//     a=b;
+//     b=temp;
+// }
+
+// for (let i=a; i<=b; i++) {
+//     if (i%2==0) evenSum +=i;
+//     else  oddSum +=i;
+
+// }
+// console.log("even sum " + evenSum ," & odd sum " + oddSum);
+    
+// Q. Print all the factors of number
+
+// let n = Number(prompt("Enter a number: "));
+
+// for (let i=1;i<=n/2;i++) {
+//     if(n%i==0) process.stdout.write(i + " , ");
+
+// } 
+// console.log(n);
+
+// Q. Check if the numbr is prime or not.
+
+// let n = Number(prompt("Enter Number"))
+
+// for()
